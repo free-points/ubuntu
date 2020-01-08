4a30c5bf9bf0545d67ed43cb7d534972705d7cba
John Amdor <JAMDORiv@gmail.com>
2020-01-08 14:07:07 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,6 +19,7 @@ if [ ! -f lynis.txt ]; then
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C80E383C3DE9F082E01391A0366C67DE91CA5D5F
 sudo apt install apt-transport-https
 echo "deb https://packages.cisofy.com/community/lynis/deb/ stable main" | sudo tee /etc/apt/sources.list.d/cisofy-lynis.list
+touch lynis.txt
 fi
 
 # Install Programs
